c7f55aa1b2ddae1702d172575ce0400e16f8d614
islam-YH <161383525+islam-YH@users.noreply.github.com>
2024-06-26 15:56:05 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Data_collector_and_monitoring
+# News and Finance Article Processing System
+
+## Overview
+
+The News and Finance Article Processing System is designed to retrieve, process, and analyze news and financial articles using modern APIs and machine learning techniques. The system fetches articles from specified news and finance APIs, preprocesses the data, converts them into embeddings using a text embedding model, and stores the embeddings in a Qdrant database for further analysis and retrieval. Additionally, it tracks the number of articles and vectors, storing this information in a MongoDB database.
+
+## Features
+
+- **Data Collection:** Fetches articles from multiple news and finance APIs (e.g., NewsAPI, GuardianAPI, GNews).
+- **Data Preprocessing:** Cleans and prepares the raw data for processing.
+- **Text Embedding:** Converts text into vector embeddings using a text embedding model (e.g., BERT).
+- **Data Storage:** Stores the embeddings in a Qdrant database for efficient retrieval.
+- **Article and Vector Count Tracking:** Tracks and stores the number of articles and vectors in a MongoDB database.
+- **Streamlit Dashboard:** Provides an interactive dashboard to visualize the number of articles and vectors over time.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.8+
+- Access to the APIs (NewsAPI, GuardianAPI, AlphaVantage API, GNews API)
+- MongoDB Atlas account
+- Qdrant account
+- Streamlit
+
+# Project Structure
+
+- collector.py: Script for collecting and processing news and financial articles.
+- dashboard.py: Streamlit dashboard for visualizing the collected data.
+- requirements.txt: List of required Python packages.
---
+++ mongo.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient # load the MongoClient class from the pymongo module
+from pymongo.server_api import ServerApi # load the ServerApi class from the pymongo.server_api module
+import pandas as pd # load the pandas module and rename it as pd
+#==================================================================================================
+# define the MongoDB URI
+MONGO_URI = "mongodb+srv://rachidmkd16:gVvZdKv4L8EArNjC@news-database.kjitsql.mongodb.net/?retryWrites=true&w=majority&appName=news-database"
+DB_NAME = 'news_database' # define the database name
+VECTOR_COUNTS_COLLECTION = 'vector_counts' # define the collection name
+ARTICLE_COUNTS_COLLECTION = 'article_counts' # define the collection name
+DAILY_ARTICLE_COUNTS_COLLECTION = 'daily_article_counts' # define the collection name
+QA_COLLECTION = 'tracking' # define the collection name
+#==================================================================================================
+client = MongoClient(MONGO_URI, server_api=ServerApi('1')) # create a MongoClient object
+db = client[DB_NAME] # create a database object
+#==================================================================================================
+def get_vector_counts(): # define a function to get the vector counts
+    cursor = db[VECTOR_COUNTS_COLLECTION].find({}) # get all the documents from the collection
+    df = pd.DataFrame(list(cursor)) # convert the cursor to a DataFrame
+    df = df.drop('_id', axis=1) # drop the _id column
+    return df # return the DataFrame
+#==================================================================================================
+def get_article_total_counts():
+    cursor = db[ARTICLE_COUNTS_COLLECTION].find({})
+    df = pd.DataFrame(list(cursor))
+    df = df.drop('_id', axis=1)
+    return df
+#==================================================================================================
+def get_daily_article_counts():
+    cursor = db[DAILY_ARTICLE_COUNTS_COLLECTION].find({})
+    df = pd.DataFrame(list(cursor))
+    df = df.drop('_id', axis=1)
+    return df
+#==================================================================================================
+def get_qa():
+    cursor = db[QA_COLLECTION].find({})
+    df = pd.DataFrame(list(cursor))
+    df = df.drop('_id', axis=1)
+    df = df[['question', 'answer', 'upvote', 'downvote', 'flag', 'timestamp']]
+    return df
+
+# ==============================================================================================================================================
